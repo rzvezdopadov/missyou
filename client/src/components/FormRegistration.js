7aeb35adff541dd32207ca05671180ec82c99046
rzvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2021-09-01 20:43:38 +0300

    update form registration for test

--- client/src/components/FormRegistration.js
+++ client/src/components/FormRegistration.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Container, Form} from 'react-bootstrap'
 
 const genInputReg = (
@@ -6,7 +6,7 @@ const genInputReg = (
         id          = "",
         inputType   = "",
         placeholder = "",
-        discription = ""
+        discription = "",
     ) => {
     return (
 <       div className="row g-3 align-items-center mt-0">
@@ -26,13 +26,75 @@ const genInputReg = (
 }
 
 export const FormRegistration = () => {
+    
+    const [eMail, setEMail] = useState(false)
+    const [pass, setPass] = useState(false)
+    const [confirmPass, setConfirmPass] = useState(false)
+
+    const setEMail = () => {
+
+
+    }
+
     return (
         <Container className="pt-3 pb-3">
             <h4>Регистрация нового пользователя:</h4>  
             <Form>
+                {/* {genInputReg("Email:","regeMail","eMail","example@example.ru")}
                 {genInputReg("Пароль:","regPassword","password","","8-20 символов (0..9, a..z, A..Z)")}
-                {genInputReg("Повторить пароль:","regConfirmPassword","password","","8-20 символов (0..9, a..z, A..Z)")}
-                {genInputReg("Email:","regeMail","eMail","example@example.ru")}
+                {genInputReg("Повторить пароль:","regConfirmPassword","password","","8-20 символов (0..9, a..z, A..Z)")} */}
+                
+                {/* Для тестов v */}
+
+                <div className="row g-3 align-items-center mt-0">
+                    <div className="col">
+                        <label htmlFor="regeMail" className="col-form-label">Email:</label>
+                    </div>
+                    <div className="col">
+                        <input 
+                            type="eMail" 
+                            id="regeMail" 
+                            className="form-control" 
+                            placeholder="example@example.com"
+                            onChange = {setEMail.bind(this.value)}  
+                        ></input>
+                    </div>
+                    <div className="col">
+                        <span id="passwordHelpInline" className="form-text">
+                        </span>
+                    </div>
+                </div>
+
+                <div className="row g-3 align-items-center mt-0">
+                    <div className="col">
+                        <label htmlFor="regPassword" className="col-form-label">Пароль:</label>
+                    </div>
+                    <div className="col">
+                        <input type="password" id="regPassword" className="form-control" placeholder=""></input>
+                    </div>
+                    <div className="col">
+                        <span id="passwordHelpInline" className="form-text">
+                            8-20 символов (0..9, a..z, A..Z)
+                        </span>
+                    </div>
+                </div>
+
+                <div className="row g-3 align-items-center mt-0">
+                    <div className="col">
+                        <label htmlFor="regConfirmPassword" className="col-form-label">Повторить пароль:</label>
+                    </div>
+                    <div className="col">
+                        <input type="password" id="regConfirmPassword" className="form-control" placeholder=""></input>
+                    </div>
+                    <div className="col">
+                        <span id="passwordHelpInline" className="form-text">
+                            8-20 символов (0..9, a..z, A..Z)
+                        </span>
+                    </div>
+                </div>
+
+                {/* Для тестов ^ */}
+
                 <div className="g-3 align-items-center mt-3">
                     <div>Нажимая на кнопку "Зарегистрироваться" вы соглашаетесь с <a href="/#">правилами</a> проекта  </div>
                     <button type="button" className="btn btn-success mt-0">Зарегистрироваться</button>
